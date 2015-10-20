2019d1f66d130d91b226469f1f296b2dcfe97cf9
Pat <patharryux@gmail.com>
2015-10-20 15:34:49 -0700

    Write tests for primary_nav

--- public/test/js/spec/secondTests.spec.js
+++ public/test/js/spec/secondTests.spec.js
@@ -5,4 +5,13 @@ describe('Testing Navigation View', function () {
   it('typeof object', function () {
     expect( typeof {} ).to.equal('object');
   });
+
+  it('app.gui.navigationView', function () {
+    expect( typeof app.gui.navigationView ).to.equal('object');
+  });
+
+  it('app.gui.navigationView', function () {
+    expect( app.gui.navigationView.$el.length ).to.equal( 1 );
+  });
+
 });
