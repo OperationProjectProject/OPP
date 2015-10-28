7ba3fe57822fc83658faa38a4edfd0563337e5c6
Pat <patharryux@gmail.com>
2015-10-27 18:43:34 -0700

    Improve preview views

--- public/js/views/profilePreviewView.js
+++ public/js/views/profilePreviewView.js
@@ -29,12 +29,22 @@ console.log("%cProfilePreviewView","color:rgba(200,200,200,1.0);font-size:1.25em
 
 
     var $profile_link = $('<a href="/#profiles/'+this.model.attributes.url_id+'" class="preview_link">');
-    var $user_name = $('<h2>').text(this.model.attributes.name);
 
-    $profile_link.append( $user_name );
+    var $text_div = $('<div class="col-sm-12 col-lg-6">');
+    var $user_name = $('<h2 class="user_name">').text(this.model.attributes.name);
+    var $user_title = $('<h3 class="user_title">').text(this.model.attributes.title);
+
+    console.log( $user_name );
+    console.log( $user_title );
+
+    [ $user_name , $user_title ].forEach( function(e,i){
+      $text_div.append( e );
+    });
+
+    $profile_link.append( $text_div );
 
     this.$el.append($profile_link);
-    
+
   } ,
   initialize: function(){
     this.listenTo(this.model, "change", this.render);
